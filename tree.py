abb120ae82216bc2919b7ca898f47dc9d9592557
İREM NUR YILDIRIM <irem.yildirim1@boun.edu.tr>
2023-12-21 12:19:20 +0300

    calculate_string function

--- tree.py
+++ tree.py
@@ -4,7 +4,7 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-
+wear_opname=["enhance","reverse","chop","trim","split"]
 MASTER = 0
 
 # Function to read and process the input file
@@ -16,7 +16,8 @@ def read_input_file(filename):
     return lines
 
 # Function to perform operations for each node
-def perform_node_operations(node_info, product):
+def calculate_string(product, operation, mod):
+    print("calculate string this is the product",product,"this is the operation",operation,"this is the mod",mod)
     #NOTES
     #Take the threshold,calculate the weariness,do the current operation and update the index for next call
     #for leaf nodes do not add
@@ -24,7 +25,6 @@ def perform_node_operations(node_info, product):
     #each node has operations list and an index value for current operation
     return
     
-
 # Master process
 num_children = {}  
 leaf_nodes = []              
@@ -53,7 +53,7 @@ if rank == MASTER:
         print("this is child",child,"this is parent",parent)
         if (child % 2 == 0) :
             operations =["enhance","split","chop"]
-            mod=3
+            mod=3 
 
         else :
             operations = ["trim","reverse"]
@@ -89,7 +89,7 @@ if rank == MASTER:
     for leaf_id, product in zip(leaf_nodes, products):
         # Send worker_info and product to the corresponding worker process
         print("sending to",leaf_id,"size i.e process number is",size,"this is i",i)
-        comm.send((node_info[leaf_id], product,node_info), dest=i) #node info gönderdim haberlesme boyle cok yer kaplıyo sanırım ama baska türlü nasıl olucak ??????????
+        comm.send((node_info[leaf_id], product,node_info,wear_factors), dest=i) #node info gönderdim haberlesme boyle cok yer kaplıyo sanırım ama baska türlü nasıl olucak ??????????
         i+=1
 
 
@@ -103,27 +103,37 @@ else:
     print("leaf nodes",leaf_nodes)
     # Receive information from the master process
     i=rank
-    node_info, product,node_list = comm.recv(source=MASTER) #from master process to worker process
+    node_info, product,node_list,wear_factors = comm.recv(source=MASTER) #from master process to worker process
     print("node info",node_info,"product",product,"leaf child is ",node_info["machine_id"])
     result = f"Result from machine {node_info['machine_id']}"
     #for real result perform the current operation without adding,leaf nodes do not add 
+    operation_performed=node_info["operations"][node_info["current_op_number"]] #current operation name
+    mod= node_info["modulo"]
+    #wear_opname=["enhance","reverse","chop","trim","split"] made this global 
+    wear_fac_index=wear_opname.index(operation_performed)
+    wear_amount=wear_factors[wear_fac_index]
+    result=calculate_string(product,operation_performed,mod)
 
     # Send the result back to the parent node, if not the root node
 
     if node_info["machine_id"] != 1:
         parent_id=node_info["parent_id"]
-        comm.send((parent_id,node_list), dest=i)   #node list yani tüm liste 
+        comm.send((parent_id,node_list,result), dest=i)   #nsend the result to parent,it will also calculate its own result and will send it to its own parent until it reaches the root node
 
 
-    parent_id,node_list= comm.recv(source=i)
+    parent_id,node_list,result= comm.recv(source=i)
     parent_info=node_list[parent_id]
-    print("Cnode info taken from child my id is ",parent_info["machine_id"],"my parent is",parent_info["parent_id"],"initial operation is",parent_info["initial_operation"])
-    # Non-leaf nodes do something specific
-    # Perform operations for each node
-        #perform_node_operations(node_info, product,num_children) #Each 
+    product=result
+    mod=parent_info["modulo"]
+    operation_performed=parent_info["operations"][parent_info["current_op_number"]]
+    wear_fac_index=wear_opname.index(operation_performed) #find the index of the operation name in the wear factors list
+    wear_amount=wear_factors[wear_fac_index]
 
-    # Perform operations for each node
-    
-        # Leaf nodes do something specific
-        # ...
+    result=calculate_string(product,operation_performed,mod) #calculate the result arrange the index 
+
+    #create a for loop for the upper part that sends messages to the parent until the root parent is reached
+
+
+    print("Cnode info taken from child my id is ",parent_info["machine_id"],"my parent is",parent_info["parent_id"],"initial operation is",parent_info["initial_operation"])
+   
       
